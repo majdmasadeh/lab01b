1c14096b4da16e5a7d3557f6963aae83d829baf0
majdmasadeh <majdmasadeh1995@gmail.com>
2021-04-27 03:55:04 +0300

    lab01b

---
+++ app.js
@@ -0,0 +1,12 @@
+'use script';
+let userName = prompt('what\'s  your name ?');
+alert (userName);
+
+let age = prompt ('what\'s your age ?');
+alert (age);
+
+let edu = prompt ('what\'s your education ?');
+alert (edu) ;
+
+let status = prompt('are you married ?');
+alert (status);
