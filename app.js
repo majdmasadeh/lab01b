24a0a81553e0603b711aa6764a58d74590094c14
majdmasadeh <majdmasadeh1995@gmail.com>
2021-05-20 13:40:37 +0300

    lab01b

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 'use script';
-let userName = prompt('what\'s  your name ?');
-alert (userName);
+let userName = prompt('what\'s  your name ? ');
+alert ('what\'s  your name ? '+userName);
+console.log('what\'s  your name ? ',userName);
 
-let age = prompt ('what\'s your age ?');
-alert (age);
+let age = prompt ('what\'s your age ? ');
+alert ('what\'s  your name ? '+userName + 'what\'s your age ? '+ age );
 
-let edu = prompt ('what\'s your education ?');
-alert (edu) ;
+console.log('what\'s your age ?',age);
+
+let edu = prompt ('what\'s your education ? ');
+alert ('what\'s  your name ? '+userName + 'what\'s your age ? '+ age  + 'what\'s your education ? ' + edu);
+console.log('what\'s your education ?',edu);
 
 let status = prompt('are you married ?');
-alert (status);
+alert ('what\'s  your name ? '+ userName + 'what\'s your age ? '+ age  + 'what\'s your education ? ' + edu + 'are you married ?' + status);
+console.log('are you married ? ',status);
